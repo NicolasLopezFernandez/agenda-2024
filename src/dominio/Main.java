1d255c6bbb68e394f25e3321388d99f1e4368683
Nicolas Lopez Fernandez <nicolas.lopezfernandez1@usp.ceu.es>
2024-11-09 17:01:22 +0100

    Inicial

---
+++ src/dominio/Main.java
@@ -0,0 +1,28 @@
+package dominio;
+import java.util.ArrayList;
+public class Agenda {
+    private String nombre;
+    private ArrayList<Contacto> lista;
+
+    public Agenda() {
+        lista = new ArrayList<>();
+        nombre = "";
+//Nombre es una referencia vacia, no nula
+    }
+
+    public Contacto buscar(Contacto c) {
+        int p = lista.inde0f(c);
+        if (p == -1)
+            return null;
+//Si la posicion es -1 entonces devuelve null porque no existe
+        else
+//El else es opcional
+            return lista.get(p);
+//Aqui devuelve el nombre de la posicion "p"
+    }
+
+    public agenda añadir(Contacto c) {
+        lista.add(c);
+        return this;
+    }
+}
